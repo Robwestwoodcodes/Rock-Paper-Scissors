6490f5b3144e619f4c341d5caa25af614f39eb15
Robert Westwood <robwestwoodcodes@gmail.com>
2023-03-01 19:30:11 +0000

    add(comments): add comments to describe functions

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
 
+function playRound (playerSelection, getComputerChoice){
+
+
+}
+
+// function that gets the user input
+// converts user input to lower case
 function playerSelection () {
     const playerStatement = ('Player has chosen ');
     let playerPrompt = prompt('Please enter either Rock, Paper or Scissors');
@@ -6,12 +13,12 @@ function playerSelection () {
     
     return playerStatement+playerCaseChange;
 }
-console.log (playerSelection());
 
+// returns a random value out of 3 choices
 function getComputerChoice () {
     const computerStatement = ('Computer has chosen ');
     let computerChoice = ['rock','paper','scissors'];
     let chosenComputerChoice = computerChoice [Math.floor(Math.random() * computerChoice.length)];
     return computerStatement+chosenComputerChoice;
 }
-console.log (getComputerChoice());   
+
