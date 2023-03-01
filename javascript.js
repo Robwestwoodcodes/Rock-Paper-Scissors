77b4ce62c6c98e03880c34a3b1b33f997cd01516
Robert Westwood <robwestwoodcodes@gmail.com>
2023-03-01 19:07:25 +0000

    feat(function):add random computer choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+
+
+
+
+
+function getComputerChoice () {
+    const computerStatment = ('Computer has chosen ');
+    let computerChoice = ['Rock','Paper','Scissors'];
+    let chosenComputerChoice = computerChoice [Math.floor(Math.random() * computerChoice.length)];
+    return computerStatment+chosenComputerChoice;
+}
+console.log (getComputerChoice());   
