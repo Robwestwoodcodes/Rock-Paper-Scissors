5dac7fe4b3c3fcf217864c120e4dbdafbf513f96
Robert Westwood <robwestwoodcodes@gmail.com>
2023-03-01 20:26:58 +0000

    fix(functions):make functions clearer

--- javascript.js
+++ javascript.js
@@ -2,19 +2,19 @@
 // function that gets the user input
 // converts user input to lower case
 function playerSelection () {
-    const playerStatement = ('Player has chosen ');
+    
     let playerPrompt = prompt('Please enter either Rock, Paper or Scissors');
     let playerCaseChange = playerPrompt.toLowerCase();
     
-    return playerStatement+playerCaseChange;
+    return playerCaseChange;
 }
 
 // returns a random value out of 3 choices
 function getComputerChoice () {
-    const computerStatement = ('Computer has chosen ');
+    
     let computerChoice = ['rock','paper','scissors'];
     let chosenComputerChoice = computerChoice [Math.floor(Math.random() * computerChoice.length)];
-    return computerStatement+chosenComputerChoice;
+    return chosenComputerChoice;
 }
 
 const userSelection = playerSelection();
@@ -22,7 +22,31 @@ const computerSelection = getComputerChoice();
 
 function playRound (userSelection, computerSelection){
 
+    const playerStatement = ('Player has chosen ');
+    const computerStatement = ('Computer has chosen ');
 
 
-}
+    if (userSelection === 'rock'){
+        rock();
+    } else if (userSelection === 'paper'){
+        paper();
+    } else (userSelection === 'scissors')
+        scissors();
+    
+    function rock (){
+        if (userSelection === computerSelection){
+        return 
+        
+    }
+
+    function paper(){
+
+    }
+
+    function scissors (){
+
+    }
+
+    } 
+
 
