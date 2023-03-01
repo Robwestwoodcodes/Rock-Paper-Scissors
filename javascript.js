20aa2d60705593c1f15db6f352102a367c13e6a9
Robert Westwood <robwestwoodcodes@gmail.com>
2023-03-01 19:27:41 +0000

    feat(function):add user input function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
 
-
-
-
+function playerSelection () {
+    const playerStatement = ('Player has chosen ');
+    let playerPrompt = prompt('Please enter either Rock, Paper or Scissors');
+    let playerCaseChange = playerPrompt.toLowerCase();
+    
+    return playerStatement+playerCaseChange;
+}
+console.log (playerSelection());
 
 function getComputerChoice () {
-    const computerStatment = ('Computer has chosen ');
-    let computerChoice = ['Rock','Paper','Scissors'];
+    const computerStatement = ('Computer has chosen ');
+    let computerChoice = ['rock','paper','scissors'];
     let chosenComputerChoice = computerChoice [Math.floor(Math.random() * computerChoice.length)];
-    return computerStatment+chosenComputerChoice;
+    return computerStatement+chosenComputerChoice;
 }
 console.log (getComputerChoice());   
