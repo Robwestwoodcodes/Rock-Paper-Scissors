ab80e0dc17a2e8708b921b85e9a27be113e967d0
Robert Westwood <robwestwoodcodes@gmail.com>
2023-03-02 20:11:18 +0000

    fix(console.log):console now returning user and computer selections correctly

--- javascript.js
+++ javascript.js
@@ -1,18 +1,20 @@
 
 // returns a random value out of 3 choices
 function getComputerChoice () {
-    
     let computerChoice = ['rock','paper','scissors'];
     let chosenComputerChoice = computerChoice [Math.floor(Math.random() * computerChoice.length)];
     return chosenComputerChoice;
 }
 
 //variables that allows the user to input a choice/ storing a function into a variable
-const playerSelection = prompt('Please enter either Rock, Paper or Scissors').toLowerCase;
+const playerSelection = prompt('Please enter either Rock, Paper or Scissors').toLowerCase();
 const computerSelection = getComputerChoice();
 
 
 //function to play a round of rock,paper or scissors
-function playRound (playerSelection, computerSelection){
+function playRound (userSelection, computerSelection){
 
     }
+
+    console.log('User selected: '+ playerSelection);
+    console.log('Computer selected: '+computerSelection);
