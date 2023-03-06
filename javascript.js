09224e8809ce08f56edd7e060ff6d02a4bb5ffbc
Robert Westwood <robwestwoodcodes@gmail.com>
2023-03-06 21:59:56 +0000

    add(logic):add completed logic to game

--- javascript.js
+++ javascript.js
@@ -1,20 +1,69 @@
 
-// returns a random value out of 3 choices
+function game(){
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
+
+for (let i = 1; i <= 5; i++){
+
 function getComputerChoice () {
     let computerChoice = ['rock','paper','scissors'];
     let chosenComputerChoice = computerChoice [Math.floor(Math.random() * computerChoice.length)];
-    return chosenComputerChoice;
+    return chosenComputerChoice
 }
 
-//variables that allows the user to input a choice/ storing a function into a variable
-const playerSelection = prompt('Please enter either Rock, Paper or Scissors').toLowerCase();
-const computerSelection = getComputerChoice();
+function userSelection (){
+let playSelection = prompt('Please enter either Rock, Paper or Scissors').toLowerCase();
+if (playSelection !== "rock" && playSelection !== "paper" && playSelection !== "scissors"){
+alert('Oops! You entered something other than/ or mis-spelled rock, paper or scissors! Please try again!')
+
+} else {
+return playSelection;
+}
+}
+
+const computerPlay = getComputerChoice();
+const playerSelection = userSelection();
 
 
-//function to play a round of rock,paper or scissors
-function playRound (userSelection, computerSelection){
 
+function playRound (computerPlay,playerSelection){
+
+        round++;
+        if (computerPlay === "rock" && playerSelection === "scissors") {
+        computerScore++;
+        return "Computer wins";
+      } else if (computerPlay === "paper" && playerSelection === "rock") {
+        playerScore++;
+        return "Player wins";
+      } else if (computerPlay === "scissors" && playerSelection === "raper") {
+        computerScore++;
+        return "Computer wins";
+      } else if (playerSelection === "rock" && computerPlay === "scissors") {
+        computerScore++;
+        return "Computer wins";
+      } else if (playerSelection === "paper" && computerPlay === "rock") {
+        playerScore++;
+        return "Player wins";
+      } else if (playerSelection === "scissors" && computerPlay === "paper") {
+        playerScore++;
+        return "Player wins";
+      } else {
+        return "Draw";
+      }
     }
 
-    console.log('User selected: '+ playerSelection);
-    console.log('Computer selected: '+computerSelection);
+ 
+
+          console.log('Rock,paper or scissors? The Game. Round: '+round);
+          console.log('User selected: '+ playerSelection);
+          console.log(`Computer selected: ${computerPlay}`);
+          console.log('Result of game: ' +playRound(computerPlay,playerSelection));
+          console.log('Player score: '+playerScore);
+          console.log('Computer score: '+computerScore);   
+          console.log(' ');
+        } 
+      }
+       game()
+
